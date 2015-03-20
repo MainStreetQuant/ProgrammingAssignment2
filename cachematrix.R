013d334637736cef2755f7fb9b9de0ad9547ed64
MainStreetQuant <JDFBIAnalytics@gmail.com>
2015-03-20 19:20:52 -0400

    Assignment #2 Response
    This is my commit, changing the forked repo as instructed.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file is my response to Week 3 
+## Programming Assignment 2
 
-## Write a short comment describing this function
+## This function is intended to cache the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  inv = NULL
+  set = function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get = function() x
+  setinv = function(inverse) inv <<- inverse 
+  getinv = function() inv
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+
+## This function is intended to invert a matrix (that is presumed to be invertible).
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  inv = x$getinv()
+  
+  if (!is.null(inv)){
+    message("fetching cached matrix...")
+    return(inv)
+  }
+  
+  mat.data = x$get()
+  inv = solve(mat.data, ...)
+  
+  x$setinv(inv)
+  
+  return(inv)
 }
+
+
